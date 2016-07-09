13624e4d639127544c8b23145cc0da8e126a421e
shmiko <shmiko@gmail.com>
2016-07-09 10:00:15 +1000

    sets

---
+++ sets.js
@@ -0,0 +1,64 @@
+function jsSet(){
+    this.set = {}; //initialise an empty object
+    this.size = 0; //initialise the size starting at 0
+}
+
+jsSet.prototype.add = function(data){
+    if (!(this.set.hasOwnProperty(data))){
+        this.set[data] = 'true';
+        this.size++;
+    }
+}
+
+jsSet.prototype.remove = function(data){
+    if (this.set.hasOwnProperty(data)){
+        delete this.set[data];
+        this.size--;
+    }
+}
+
+var testMySet = new jsSet();
+testMySet.add(1);
+testMySet.add(2);
+testMySet.add(3);
+console.log('print testMySet ',testMySet);
+console.log('removing 2');
+testMySet.remove(2);
+console.log('print testMySet ',testMySet);
+
+
+jsSet.prototype.exists = function(data){
+    if (this.set.hasOwnProperty(data)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+jsSet.prototype.union = function(setTwo){
+    var unionSet = new jsSet();
+    for (var key in this.set){
+        if (this.set.hasOwnProperty(key)){
+            unionSet.add(key);
+        }
+    }
+    for (var key in setTwo.set){
+        if (!unionSet.set.hasOwnProperty(key)){
+            unionSet.add(key);
+        }
+    }
+    return unionSet;
+}
+
+jsSet.prototype.intesect = function(setTwo){
+    var interSet = new jsSet();
+    for (var key in this.set){
+        if (setTwo.set.hasOwnProperty(key)){
+            interSet.add(key);
+        }
+    }
+    return interSet;
+}
+
+
+
