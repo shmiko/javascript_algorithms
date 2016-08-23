4de233269de5b43483ae1d507011599720d58752
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-08-23 13:12:51 +1000

    summ all

---
+++ summAll.js
@@ -0,0 +1,30 @@
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
+function getMinOfArray(numArray) {
+  return Math.min.apply(null, numArray);
+}
+
+
+function sumAll2(arr) {
+	var total = 0;
+	var i = 0;
+	var sortedArray = [];
+	var currNum =  0;
+	for (i = getMinOfArray(arr); i <= getMaxOfArray(arr); i++){
+		currNum = i;
+		total = total + currNum;
+		console.log(total);
+	};
+	console.log("Grand Total is",total);
+	return total;
+}
+
+sumAll2([1, 4]);
+
+
+console.log('2nd total is ', sumAll2([4,1]));
+
+var checkSum = sumAll2([5,10]);
+console.log('3rd total is ', checkSum);
