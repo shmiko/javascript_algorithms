f9aaf1d98e84d3a53120b8429bbcc7af653f0f9a
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-10-05 14:26:20 +1100

    front end te3sts

--- test.js
+++ test.js
@@ -5,11 +5,24 @@ var add = function(a,b){
 	console.log(add(4,3));
 	var str  = "i'm a lasagna hog".split("").reverse().join("");
 	console.log(str);
-	
-	
-var foo = "Hello";
-(function() {
-  var bar = " World";
-  alert(foo + bar);
-})();
-console.log(foo + bar);
+
+
+// var foo = "Hello";
+// (function() {
+//   var bar = " World";
+//   console.log(foo + bar);
+// })();
+// console.log(foo + bar);
+
+console.log('one');
+setTimeout(function() {
+  console.log('two');
+}, 0);
+console.log('three');
+
+var foo = {n: 1};
+console.log(foo);
+var bar = foo;
+console.log(bar);
+foo.x = foo = {n: 2};
+console.log(foo.x);
