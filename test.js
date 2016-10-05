d5553a96ba888f98ca46051a2fc1783706b5d7a2
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-10-05 14:05:13 +1100

    js tests

---
+++ test.js
@@ -0,0 +1,15 @@
+var add = function(a,b){
+	return a + b;
+	}
+
+	console.log(add(4,3));
+	var str  = "i'm a lasagna hog".split("").reverse().join("");
+	console.log(str);
+	
+	
+var foo = "Hello";
+(function() {
+  var bar = " World";
+  alert(foo + bar);
+})();
+console.log(foo + bar);
