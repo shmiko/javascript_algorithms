58167136c7d1ccf7e5ec35ca67c97188a3c7a2f2
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-10-05 14:53:57 +1100

    js tests

--- test.js
+++ test.js
@@ -38,6 +38,33 @@ var add = function(x){
 
 console.log(add(2)(3));
 
+// Initialize a string variable for the output
+var output = '';
+
+// Count to 100 using i as the counter
+for (var i = 1; i <= 100; i++) {
+
+    // If i is divisible by 3 with no remainder, append Fizz
+    if (i % 3 === 0) {
+        output += 'Fizz ';
+    }
+
+    // If i is divisible by 5 with no remainder, append Buzz
+    if (i % 5 === 0) {
+        output += 'Buzz ';
+    }
+
+    // If i is not divisible by 3 or 5, append the number itself.
+    // Note: JavaScript evaluates non-zero numbers as true:
+    // so, if both divisors yield a remainder, the expression is true.
+    if (i % 3 && i % 5) {
+        output += i + ' ';
+    }
+}
+
+// Print the output to the console
+console.log(output);
+
 var o='';
 for (var i=1; i<=100; i++) {
     i%3 || (o+='Fizz ');
