f98c4bc620e906c3b7bef84ac7dd08eaef168dcc
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-10-06 12:09:57 +1100

    js tests

--- test.js
+++ test.js
@@ -208,3 +208,33 @@ function fib(n, undefined){
     return fib.cache[n];
 }
 fib.cache = [0, 1, 1];
+
+
+Function.prototype.memoize = function() {
+	var self = this, cache = {};
+    return function(num) {
+    	if(cache[num]) {
+            console.log('cache hit!');
+            return cache[num];
+        } else {
+            console.log('computing fibonacci');
+            return cache[num] = self(num);
+        }
+    };
+}
+
+var fib = function(n) {
+	var a=0, b=1, f=1;
+    for(var i=2; i<n; i++) {
+    	f = a+b;
+        a = b;
+        b = f;
+    }
+    return f;
+}
+
+var fastFib = fib.memoize();
+//check your browser console
+console.log(fastFib(10));
+console.log(fastFib(13));
+console.log(fastFib(10));
