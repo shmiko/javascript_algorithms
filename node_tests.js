3e4569af8609ebaedd075bdd8e38da449d530311
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-12-20 18:23:33 +1100

    tidy

--- node_tests.js
+++ node_tests.js
@@ -1,10 +1,10 @@
 for (var i = 0;i < 12; i++){
 	setInterval(function() {
-	console.log("foo", i);
-	//while (true) {console.log("still foo");}
+		console.log("foo", i);
+		//while (true) {console.log("still foo");}
 	}, 1000);
 	setInterval(function() {
-	console.log("bar", i);
+		console.log("bar", i);
 	}, 1000);
 	i++;
 }
@@ -12,8 +12,8 @@ for (var i = 0;i < 12; i++){
 var fs = require("fs");
 fs.readFile("foo.txt", "utf8", function(error, data) {
 if (error) {
-throw error;
+	throw error;
 }
-console.log(data);
+	console.log(data);
 });
 console.log("Reading file...");
