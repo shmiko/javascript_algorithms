0bde8bf4fa746feb8e6960efd17dad2e0310913c
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-12-20 12:41:15 +1100

    readFile

---
+++ node_tests.js
@@ -0,0 +1,19 @@
+for (var i = 0;i < 12; i++){
+	setInterval(function() {
+	console.log("foo", i);
+	//while (true) {console.log("still foo");}
+	}, 1000);
+	setInterval(function() {
+	console.log("bar", i);
+	}, 1000);
+	i++;
+}
+
+var fs = require("fs");
+fs.readFile("foo.txt", "utf8", function(error, data) {
+if (error) {
+throw error;
+}
+console.log(data);
+});
+console.log("Reading file...");
